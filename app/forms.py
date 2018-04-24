877e458870f348de8a50ced4a19981f6b308c2e8
Richard Leblanc <rich@vmfreebsd11.1>
2018-04-24 04:49:16 -0700

    Initial commit

---
+++ app/forms.py
@@ -0,0 +1,16 @@
+'''
+Created on Apr 13, 2018
+
+@author: rich
+'''
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+    
