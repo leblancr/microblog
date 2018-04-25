b1bec8d26fb21dc563673d3a368be854f9d1c4ec
rich <rich@vmfreebsd11>
2018-04-25 10:02:41 -0700

    Finished chapter 4 database

--- microblog.py
+++ microblog.py
@@ -1,4 +1,9 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
 if __name__ == '__main__':  # Script executed directly?
     app.run()  # Launch built-in web server and run this Flask webapp
