8c1b4e2a6a1565fd44f96e83c3b5863e264baef5
rich <rich@vmfreebsd11>
2018-04-25 22:18:36 -0700

    Finished ch 5 login

--- app/models.py
+++ app/models.py
@@ -5,15 +5,16 @@ Created on Apr 14, 2018
 '''
 
 from datetime import datetime
-from app import db
+from app import db, migrate, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # @UndefinedVariable
+    username = db.Column(db.String(64), index=True, unique=True)  # @UndefinedVariable
+    email = db.Column(db.String(120), index=True, unique=True)  # @UndefinedVariable
+    password_hash = db.Column(db.String(128))  # @UndefinedVariable
+    posts = db.relationship('Post', backref='author', lazy='dynamic')  # @UndefinedVariable
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -24,11 +25,15 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+@login.user_loader
+def load_user(userid):
+    return User.query.get(int(userid))
+
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)  # @UndefinedVariable
+    body = db.Column(db.String(140))  # @UndefinedVariable
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # @UndefinedVariable
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # @UndefinedVariable
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
