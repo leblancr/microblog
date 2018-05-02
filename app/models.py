dab004bbfac74f153f64b6a187a284eed5397720
rich <rich@vmfreebsd11>
2018-05-02 09:51:09 -0700

    After chapter 8 followers

--- app/models.py
+++ app/models.py
@@ -10,6 +10,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)    
+
 class User(UserMixin, db.Model):
     # This class represents a database table.
     id = db.Column(db.Integer, primary_key=True)  # @UndefinedVariable
@@ -19,7 +24,12 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')  # @UndefinedVariable
     about_me = db.Column(db.String(140))  # @UndefinedVariable
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)  # @UndefinedVariable
-    
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
     
@@ -32,6 +42,25 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0 
+            
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())            
 
 @login.user_loader
 def load_user(userid):
@@ -45,3 +74,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+    
